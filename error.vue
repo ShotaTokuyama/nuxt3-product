ba1b5e4156dd6b81972a90693b2fb31c67e1425c
tokuyama_shota <tokush1031@gmail.com>
2024-02-22 17:20:26 +0900

    error page add

---
+++ error.vue
@@ -0,0 +1,13 @@
+<script setup>
+const error = useError();
+</script>
+<template>
+  <div class="text-center mt-40">
+    <h1 v-if="error.statusCode === 404" class="text-6xl">
+      こちらのページは存在しません
+    </h1>
+    <h1 v-else>何らかのErrorが発生しています。</h1>
+    <br />
+    <div class="text-3xl">{{ error.message }}</div>
+  </div>
+</template>
